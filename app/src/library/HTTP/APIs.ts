f5624b37584904c9e5bbe05007fbe82758030b2d
mb4ndeira <mbandeira.cs@gmail.com>
2023-08-10 13:09:42 -0300

    chore: fix next prefix on enviroment variables

--- app/src/library/HTTP/APIs.ts
+++ app/src/library/HTTP/APIs.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const toDo = axios.create({
-  baseURL: process.env.NEXT_TODO_API_URL || "http://localhost:3030",
+  baseURL: process.env.NEXT_PUBLIC_TODO_API_URL || "http://localhost:3030",
 });
