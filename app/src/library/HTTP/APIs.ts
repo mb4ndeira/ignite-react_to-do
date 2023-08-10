66d8713c7a0ee6d90d28642c717270d921123b91
mb4ndeira <mbandeira.cs@gmail.com>
2023-08-10 13:01:29 -0300

    chore: debug env variables in vercel

--- app/src/library/HTTP/APIs.ts
+++ app/src/library/HTTP/APIs.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+console.log(
+  process.env,
+  process.env.NEXT_TODO_API_URL,
+  process.env.TODO_API_URL,
+);
+
 export const toDo = axios.create({
   baseURL: process.env.NEXT_TODO_API_URL || "http://localhost:3030",
 });
