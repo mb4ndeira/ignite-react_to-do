2a07509e693c8f1628af172c9a32c52b8e50c809
mb4ndeira <mbandeira.cs@gmail.com>
2023-08-10 12:09:18 -0300

    fix: todo api environment variable

--- app/src/library/HTTP/APIs.ts
+++ app/src/library/HTTP/APIs.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const toDo = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? process.env.TODO_API_URL
-      : "http://localhost:3030",
+  baseURL: process.env.TODO_API_URL || "http://localhost:3030",
 });
